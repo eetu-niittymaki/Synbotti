ecd0792c8cf0cf0162d64bf096e86c63108219df
eetu-niittymaki <eetu945@gmail.com>
2022-10-29 10:54:40 +0300

    Add file writing/reading, setInterval

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+/*////////////////////////////////////////
+//     Requires Node 16.9 or higher!    //
+////////////////////////////////////////*/
+
 const { Client, GatewayIntentBits } = require("discord.js")
 const dotenv = require("dotenv")
 const fs = require("fs")
 const gdrive = require('./gdriveServer.js')
 const client = new Client({
     intents: [ 
-        //GatewayIntentBits.Guilds,
-		//GatewayIntentBits.GuildMessages,
-		//GatewayIntentBits.MessageContent, 
-        //GatewayIntentBits.DirectMessages, 
+        GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent, 
+        GatewayIntentBits.DirectMessages, 
     ]
   })
 
@@ -18,9 +22,14 @@ const file = "log.txt"
 
 // Runs once on bot startup
 client.once("ready", async () => {
-    //const data = fs.readFileSync(file, 'utf-8') 
-    data = await gdrive.getMembers()
-    count = data
+    const data = fs.readFileSync(file, 'utf-8') 
+    let toString = data.toString()
+    let lines = toString.split("\n")
+    if (lines.length-1 > 0) {
+        count = Number(lines)
+    } else {
+        count = await gdrive.getMembers()
+    }
     console.log("count = ",count)
     console.log("Ready!")
 })
@@ -31,19 +40,21 @@ const readSheet = async () => {
     if (countMem > count) {
         count = countMem
         sendMessage(countMem)
-        /*fs.writeFile(file, countMem, (err) => {
+        fs.writeFile(file, countMem, (err) => {
             if (err) return console.log(err)
-        })*/
+        })
+    } else {
+        console.log("No new members")
     }
 }
 
-setTimeout(readSheet, 1000 * 60 * 60 * 12)
-
 const sendMessage = (countMem) => {
-    client.on('ready', async () => {
+    client.on('ready', async guild => {
         const channel =  await guild.channels.array().filter(c => c.name.toLowerCase() === 'synbotti')
-        await channel.send(`Uusia jäseniä: ${countMem-count}`)
+        await channel.send({content: `Uusia jäseniä: ${countMem-count}`,})
     })
 }
+
+setInterval(readSheet, 1000 * 60 * 60 * 12)
 
 client.login(process.env.TOKEN)
