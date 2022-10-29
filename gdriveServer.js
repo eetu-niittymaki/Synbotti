a725180946709a274e212a4f2e6bc334878b1bcc
eetu-niittymaki <eetu945@gmail.com>
2022-10-29 14:03:32 +0300

    Change two gdrive requests with for loop

--- gdriveServer.js
+++ gdriveServer.js
@@ -4,8 +4,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 // Sheet ID
-const sheet1 = process.env.SHEET_ONE
-const sheet2 = process.env.SHEET_TWO
+const sheetArray = [process.env.SHEET_ONE, process.env.SHEET_TWO]
 
 // Authenticates connection to Google Sheets
 const authentication = async () => {
@@ -28,17 +27,15 @@ const authentication = async () => {
 const getMembers = async () => {
   try {
     const { sheets } = await authentication()
-
-    const res1 = await sheets.spreadsheets.values.get({
-      spreadsheetId: sheet1 ,
-      range: "A:Z",
-    })
-
-    const res2 = await sheets.spreadsheets.values.get({
-      spreadsheetId: sheet2,
-      range: "A:Z",
-    })
-    return res1.data.values.length + res2.data.values.length 
+    let count =0
+    for (let i=0; i < sheetArray.length; i++) {
+      const res = await sheets.spreadsheets.values.get({
+        spreadsheetId: sheetArray[i],
+        range: "A:Z",
+      })
+      count += res.data.values.length
+    }
+    return count
   } catch(e) {
     console.log(e)
   }
